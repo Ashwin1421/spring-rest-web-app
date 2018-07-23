c3603aaee10939020472ce1c9c864258a5a1eced
Ashwin Joshi <ashwin.joshi12@live.com>
2018-07-23 18:33:13 -0500

    Initial Commit

---
+++ src/test/java/com/ashwin/AppTest.java
@@ -0,0 +1,20 @@
+package com.ashwin;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
