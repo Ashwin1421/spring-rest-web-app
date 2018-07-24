0641c2934cc06398dd4c7f5c5b6970b33051c130
Ashwin Joshi <ashwin.joshi12@live.com>
2018-07-24 01:41:18 -0500

    added:entities- vehicle, user, weather-sensor

---
+++ src/main/java/com/ashwin/entity/Wind.java
@@ -0,0 +1,25 @@
+package com.ashwin.entity;
+
+import javax.persistence.Embeddable;
+
+@Embeddable
+public class Wind {
+    private int speed;
+    private int degree;
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    public int getDegree() {
+        return degree;
+    }
+
+    public void setDegree(int degree) {
+        this.degree = degree;
+    }
+}
