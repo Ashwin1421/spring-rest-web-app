32d01ac3bada18727ff11b396606a87481f53305
Ashwin Joshi <ashwin.joshi12@live.com>
2018-07-24 01:44:25 -0500

    updated:SpringApplication

--- src/main/java/com/ashwin/App.java
+++ src/main/java/com/ashwin/App.java
@@ -1,13 +1,16 @@
 package com.ashwin;
 
-/**
- * Hello world!
- *
- */
-public class App 
+import com.ashwin.entity.Vehicle;
+import com.ashwin.repository.VehicleRepository;
+import com.ashwin.service.VehicleService;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ){
+        SpringApplication app = new SpringApplication(App.class);
+        app.run();
     }
 }
