9bc3a2c4ec0aeeae4f5aecb77f66caa93846a77a
Ashwin Joshi <ashwin.joshi12@live.com>
2018-07-24 01:43:38 -0500

    added:VehicleController

---
+++ src/main/java/com/ashwin/controller/VehicleController.java
@@ -0,0 +1,64 @@
+package com.ashwin.controller;
+
+import com.ashwin.entity.Vehicle;
+import com.ashwin.service.VehicleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:8080/vehicles")
+@RequestMapping(value = "/vehicles")
+public class VehicleController {
+
+    @Autowired
+    VehicleService vehicleService;
+
+    @RequestMapping(
+            method = RequestMethod.GET,
+            produces = MediaType.APPLICATION_JSON_UTF8_VALUE
+    )
+    public List<Vehicle> findAll(){
+        return vehicleService.findAll();
+    }
+
+    @RequestMapping(
+            method = RequestMethod.GET,
+            value = "/{id}",
+            produces = MediaType.APPLICATION_JSON_UTF8_VALUE
+    )
+    public Vehicle findById(@PathVariable("id") String id){
+        return vehicleService.findOne(id);
+    }
+
+    @RequestMapping(
+            method = RequestMethod.POST,
+            consumes = MediaType.APPLICATION_JSON_UTF8_VALUE,
+            produces = MediaType.APPLICATION_JSON_UTF8_VALUE
+    )
+    public Vehicle create(@RequestBody Vehicle newVehicle){
+        return vehicleService.create(newVehicle);
+    }
+
+
+    @RequestMapping(
+            method = RequestMethod.PUT,
+            value = "/{id}",
+            consumes = MediaType.APPLICATION_JSON_UTF8_VALUE,
+            produces = MediaType.APPLICATION_JSON_UTF8_VALUE
+    )
+    public Vehicle update(@PathVariable("id") String id, Vehicle updatedVehicle){
+        return vehicleService.update(id, updatedVehicle);
+    }
+
+
+    @RequestMapping(
+            method = RequestMethod.DELETE,
+            value = "/{id}"
+    )
+    public void delete(@PathVariable("id") String id){
+        vehicleService.delete(id);
+    }
+}
