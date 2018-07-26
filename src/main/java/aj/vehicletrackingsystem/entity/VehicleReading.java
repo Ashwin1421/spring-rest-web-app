4274543d9871eaac8c3c8e3bcfc93db53910b31c
Ashwin Joshi <ashwin.joshi12@live.com>
2018-07-26 01:12:14 -0500

    added:VehicleReading.java

---
+++ src/main/java/aj/vehicletrackingsystem/entity/VehicleReading.java
@@ -0,0 +1,126 @@
+package aj.vehicletrackingsystem.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import javax.persistence.*;
+
+@Entity
+public class VehicleReading {
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "vehicle_id", nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JsonIgnore
+    private String vin;
+
+    private float latitude;
+    private float longitude;
+    private String timestamp;
+    private float fuelVolume;
+    private int speed;
+    private int engineHp;
+    private boolean checkEngineLightOn;
+    private boolean engineCoolantLow;
+    private boolean cruiseControlOn;
+    private int engineRpm;
+    private Tire tires;
+
+
+    public Tire getTires() {
+        return tires;
+    }
+
+    public void setTires(Tire tires) {
+        this.tires = tires;
+    }
+
+    public int getEngineRpm() {
+        return engineRpm;
+    }
+
+    public void setEngineRpm(int engineRpm) {
+        this.engineRpm = engineRpm;
+    }
+
+    public boolean isCruiseControlOn() {
+        return cruiseControlOn;
+    }
+
+    public void setCruiseControlOn(boolean cruiseControlOn) {
+        this.cruiseControlOn = cruiseControlOn;
+    }
+
+    public boolean isEngineCoolantLow() {
+        return engineCoolantLow;
+    }
+
+    public void setEngineCoolantLow(boolean engineCoolantLow) {
+        this.engineCoolantLow = engineCoolantLow;
+    }
+
+    public boolean isCheckEngineLightOn() {
+        return checkEngineLightOn;
+    }
+
+    public void setCheckEngineLightOn(boolean checkEngineLightOn) {
+        this.checkEngineLightOn = checkEngineLightOn;
+    }
+
+    public int getEngineHp() {
+        return engineHp;
+    }
+
+    public void setEngineHp(int engineHp) {
+        this.engineHp = engineHp;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    public float getFuelVolume() {
+        return fuelVolume;
+    }
+
+    public void setFuelVolume(float fuelVolume) {
+        this.fuelVolume = fuelVolume;
+    }
+
+    public String getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(String timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public float getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(float longitude) {
+        this.longitude = longitude;
+    }
+
+    public float getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(float latitude) {
+        this.latitude = latitude;
+    }
+
+    public String getVin() {
+        return vin;
+    }
+
+    public void setVin(String vin) {
+        this.vin = vin;
+    }
+}
